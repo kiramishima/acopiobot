62afdb86823cff2c0e15df0724ed37f9cb177d03
Paul Arizpe <paul@iterando.mx>
2017-09-21 00:52:28 -0500

    Fix PORT

--- app.js
+++ app.js
@@ -125,7 +125,7 @@ const server = restify.createServer();
 server.use(restify.plugins.acceptParser(server.acceptable));
 server.use(restify.plugins.queryParser());
 server.use(restify.plugins.bodyParser()); 
-server.listen(process.env.port || 3978, function () {
+server.listen(process.env.PORT || 3978, function () {
     console.log('%s listening to %s', server.name, server.url);
 });
 server.post('/api/messages', connector.listen());
