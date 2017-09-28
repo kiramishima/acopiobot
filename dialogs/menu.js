c440f2b7ce5d0330d834978d243c3b3f8c288964
Paul Arizpe <paul@iterando.mx>
2017-09-28 12:46:35 -0500

    Test error de FB con 2 send messages

--- dialogs/menu.js
+++ dialogs/menu.js
@@ -3,8 +3,10 @@ const builder = require("botbuilder");
 module.exports = function (bot) {
     bot.dialog('/menu', [
         (session) => {
-            session.send("A continuación encontrarás diferentes organizaciones, centros de acopio o formas de ayudar a los afectados o rescatistas del sismo.");
-            session.send("Si conoces alguna otra forma de ayudar Contáctanos en Twitter @comoayudarmx o envianos un email a comoayudarmx@gmail.com");
+            let message = "A continuación encontrarás diferentes organizaciones, centros de acopio o formas de ayudar a los afectados o rescatistas del sismo.\nSi conoces alguna otra forma de ayudar Contáctanos en Twitter @comoayudarmx o envianos un email a comoayudarmx@gmail.com ";
+            session.send(message); // 2 send generate FB Error
+            //session.send("A continuación encontrarás diferentes organizaciones, centros de acopio o formas de ayudar a los afectados o rescatistas del sismo.");
+            //session.send("Si conoces alguna otra forma de ayudar Contáctanos en Twitter @comoayudarmx o envianos un email a comoayudarmx@gmail.com");
             var choices = "Listado general|Filtrado por locación|Filtrado por tipo de donacion|Ver FakeNews";
             builder.Prompts.choice(session, "Que accion le gustaria realizar?", choices, { listStyle: builder.ListStyle.button });
         },
